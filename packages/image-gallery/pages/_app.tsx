20ddc00bcc6543e91953cd657fa94368dc073256
j- <j@skeoh.com>
2021-05-22 13:36:06 +1000

    Add store to UI

--- packages/image-gallery/pages/_app.tsx
+++ packages/image-gallery/pages/_app.tsx
@@ -1,8 +1,16 @@
 import { AppProps } from 'next/app';
+import { createStore } from 'redux';
+import rootReducer from 'image-store';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider as StoreProvider } from 'react-redux';
 import '../styles/globals.css';
 
+const store = createStore(rootReducer, composeWithDevTools());
+
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <StoreProvider store={store}>
+    <Component {...pageProps} />
+  </StoreProvider>
 );
 
 export default MyApp;
