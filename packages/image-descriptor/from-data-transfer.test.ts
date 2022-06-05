cd486d05d10013bc1d280ac3d7349a5e1a182d77
j- <j@skeoh.com>
2022-06-05 20:50:50 +1000

    Fix failing tests

--- packages/image-descriptor/from-data-transfer.test.ts
+++ packages/image-descriptor/from-data-transfer.test.ts
@@ -2,25 +2,25 @@ import { mockDataTransfer } from './__test__';
 import { getImageDescriptorsFromDataTransfer } from './from-data-transfer';
 import { THROW_IF_EMPTY } from './types';
 
-it('returns empty array if given fresh data transfer object when flag is unset', () => {
+it('returns empty array if given fresh data transfer object when flag is unset', async () => {
   const dt = mockDataTransfer();
-  const actual = getImageDescriptorsFromDataTransfer(dt);
+  const actual = await getImageDescriptorsFromDataTransfer(dt);
   expect(actual).toEqual([]);
 });
 
-it('throws if given fresh data transfer object when flag is set', () => {
+it('throws if given fresh data transfer object when flag is set', async () => {
   const dt = mockDataTransfer();
-  expect(() => {
-    getImageDescriptorsFromDataTransfer(dt, THROW_IF_EMPTY);
-  }).toThrow('Could not find any images in data transfer');
+  await expect(() => (
+    getImageDescriptorsFromDataTransfer(dt, THROW_IF_EMPTY)
+  )).rejects.toThrow('Could not find any images in data transfer');
 });
 
-it('returns DT from URI list', () => {
+it('returns DT from URI list', async () => {
   const dt = mockDataTransfer({
     types: ['text/uri-list'],
   });
   dt.getData.mockImplementation(() => 'https://upload.wikimedia.org/wikipedia/commons/b/bb/Alexander_cuts_the_Gordian_Knot.jpg');
-  const actual = getImageDescriptorsFromDataTransfer(dt);
+  const actual = await getImageDescriptorsFromDataTransfer(dt);
   expect(actual).toEqual([
     {
       url: 'https://upload.wikimedia.org/wikipedia/commons/b/bb/Alexander_cuts_the_Gordian_Knot.jpg',
