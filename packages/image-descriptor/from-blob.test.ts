786ac7fb6d22337044259d234b30f182cbc2eb6a
j- <j@skeoh.com>
2022-06-13 22:46:37 +1000

    Define "create object URL" in test setup

--- packages/image-descriptor/from-blob.test.ts
+++ packages/image-descriptor/from-blob.test.ts
@@ -2,18 +2,8 @@ import { RAW_1_BY_1_TRANSPARENT_PIXEL_PNG } from './__test__';
 import { getImageDescriptorFromBlob } from './from-blob';
 import { dataURIToBlob } from './utils';
 
-const createObjectURL = jest.fn();
-
-beforeEach(() => {
-  jest.clearAllMocks();
-  Object.defineProperty(URL, 'createObjectURL', {
-    writable: true,
-    value: createObjectURL,
-  });
-});
-
 it('returns type and object URL', () => {
-  createObjectURL.mockReturnValue('blob:http://localhost/33d9d39c-0987-4aaa-a3c8-326ba9b36d80');
+  jest.mocked(URL.createObjectURL).mockReturnValue('blob:http://localhost/33d9d39c-0987-4aaa-a3c8-326ba9b36d80');
   const blob = dataURIToBlob(RAW_1_BY_1_TRANSPARENT_PIXEL_PNG);
   const actual = getImageDescriptorFromBlob(blob);
   expect(actual).toEqual({
--- packages/image-descriptor/setupTests.ts
+++ packages/image-descriptor/setupTests.ts
@@ -3,3 +3,10 @@ export {};
 beforeEach(() => {
   jest.spyOn(console, 'debug').mockImplementation(() => {});
 });
+
+beforeEach(() => {
+  Object.defineProperty(URL, 'createObjectURL', {
+    writable: true,
+    value: jest.fn(),
+  });
+});
