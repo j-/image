b7c866e3cdf2821602d8bdb3d46e805a33e1f579
j- <j@skeoh.com>
2022-06-13 22:47:12 +1000

    Implement "is text media type" in "from clipboard item"

--- packages/image-descriptor/from-clipboard-item.ts
+++ packages/image-descriptor/from-clipboard-item.ts
@@ -2,14 +2,14 @@ import { ALLOW_ALL_TYPES, ImageDescriptor, ImageDescriptorFlags, THROW_IF_EMPTY 
 import { getImageDescriptorsFromString } from './from-string';
 import { getImageDescriptorFromBlob } from './from-blob';
 import { assertNotEmpty } from './assert';
-import { isImageMediaType } from './utils';
+import { isImageMediaType, isTextMediaType } from './utils';
 
 export const getImageDescriptorsFromClipboardItem = async (clipboardItem: ClipboardItem, flags: ImageDescriptorFlags = 0): Promise<ImageDescriptor[]> => {
   console.debug('getImageDescriptorsFromClipboardItem');
   const results: ImageDescriptor[] = [];
   for (const type of clipboardItem.types) {
     const blob = await clipboardItem.getType(type);
-    if (/^text\//.test(type)) {
+    if (isTextMediaType(type)) {
       const text = await blob.text();
       results.push(...getImageDescriptorsFromString(text, flags & ~THROW_IF_EMPTY));
     } else if (isImageMediaType(type) || flags & ALLOW_ALL_TYPES) {
