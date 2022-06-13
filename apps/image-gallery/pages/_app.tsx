3e84b2313cb18bc32bb579805a01891d4d615478
j- <j@skeoh.com>
2022-06-13 15:52:44 +1000

    Revoke object URLs whenever they are removed from the store

--- apps/image-gallery/pages/_app.tsx
+++ apps/image-gallery/pages/_app.tsx
@@ -1,11 +1,30 @@
 import { AppProps } from 'next/app';
-import { createStore } from 'redux';
-import rootReducer from 'image-store';
+import { applyMiddleware, createStore, Middleware } from 'redux';
+import rootReducer, { getAllImageURLs } from 'image-store';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider as StoreProvider } from 'react-redux';
 import '../styles/globals.css';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const middleware: Middleware[] = [
+  /** Revoke object URLs whenever they are removed from the store. */
+  (store) => (next) => (action) => {
+    const before = getAllImageURLs(store.getState());
+    next(action);
+    const after = getAllImageURLs(store.getState());
+    for (const url of before) {
+      if (!after.includes(url)) {
+        URL.revokeObjectURL(url);
+      }
+    }
+  },
+];
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(...middleware)
+  )
+);
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
   <StoreProvider store={store}>
